6cbf5d3eba8ccf6d5360d364755ae0a88a0754e6
DENIZ <deniz.kaymal@gmail.com>
2020-11-15 23:17:44 -0500

    project is already in remote repo , committing more changes

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,13 @@
 public class Hello {
     //more stuff
     // Mac user and windows users are happy to see commit dialog finally
-    public static void main(String[] args) {
 
-    }
+    // I have already published on GitHub.com
+    // now i am ready to make more commits
+    // but do i need to share again
+    // Absolutely NO
+    //IT'S TIME COMMIT AND JUST PUSH!!!!!
+
+
+
 }
